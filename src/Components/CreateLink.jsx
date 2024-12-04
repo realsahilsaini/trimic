8e850553cb2d129db6124ec5c43d369724fbb73a
realsahilsaini <thisissahilsaini@gmail.com>
2024-12-04 23:36:41 +0530

    Import fixed

--- src/Components/CreateLink.jsx
+++ src/Components/CreateLink.jsx
@@ -9,7 +9,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/Components/ui/dialog"
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 import Error from './Error';
